09f683307a3b39aa252339aa276febd3a383cf72
sbsmrth <andresf.mrh@gmail.com>
2025-09-15 11:06:51 -0500

    refactor(core): remove default app controller and service, add ProjectsModule, update eslint rules, and add @nestjs/mapped-types dependency

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ProjectsModule } from './projects/projects.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ProjectsModule],
 })
 export class AppModule {}
